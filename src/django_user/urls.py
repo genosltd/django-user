49437ee1a2defb24334dbc6a27627f37cc7891ad
Genadij Razdorov <genadijrazdorov@gmail.com>
2022-06-23 11:07:17 +0200

    Add password reset functionality

--- src/django_user/urls.py
+++ src/django_user/urls.py
@@ -13,12 +13,30 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
 
-from django_user.urls import urlpatterns as du_urlpatterns
+from django.contrib.auth import views as auth_views
 
 
-urlpatterns = du_urlpatterns + [
-    path('admin/', admin.site.urls),
+urlpatterns = [
+    path(
+        'admin/password_reset/',
+        auth_views.PasswordResetView.as_view(),
+        name='admin_password_reset',
+    ),
+    path(
+        'admin/password_reset/done/',
+        auth_views.PasswordResetDoneView.as_view(),
+        name='password_reset_done',
+    ),
+    path(
+        'reset/<uidb64>/<token>/',
+        auth_views.PasswordResetConfirmView.as_view(),
+        name='password_reset_confirm',
+    ),
+    path(
+        'reset/done/',
+        auth_views.PasswordResetCompleteView.as_view(),
+        name='password_reset_complete',
+    ),
 ]
