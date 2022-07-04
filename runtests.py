67a2774c1bf61fabb506626abf984c0958be45cc
Genadij Razdorov <genadijrazdorov@gmail.com>
2022-07-04 13:48:32 +0200

    Modify runtests Runner.__init__ params

--- runtests.py
+++ runtests.py
@@ -10,6 +10,14 @@ if __name__ == "__main__":
     os.environ['DJANGO_SETTINGS_MODULE'] = 'tests.test_settings'
     django.setup()
     TestRunner = get_runner(settings)
-    test_runner = TestRunner()
+
+    # https://docs.djangoproject.com/en/4.0/topics/testing/advanced/#defining-a-test-runner
+    test_runner = TestRunner(
+        verbosity=2,
+        interactive=True,
+        failfast=False,
+        debug_sql=False,
+        pdb=True,
+    )
     failures = test_runner.run_tests(["tests"])
     sys.exit(bool(failures))
